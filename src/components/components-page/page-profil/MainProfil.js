2a6faefe41579f43f6c3db2511530a76f878b926
AaronSaada <aaron.saada0@gmail.com>
2023-08-08 16:28:51 +0200

    Ajout du profil

--- src/components/components-page/page-profil/MainProfil.js
+++ src/components/components-page/page-profil/MainProfil.js
@@ -1,7 +1,23 @@
-
+import PhotoDeProfil from '../../../images/photo-de-profil.png'
 
 export default function MainProfil(){
-    <div>
-        
-    </div>
+    return(
+        <>
+            <div id='profile-informations-container'>
+                <div id='profile-informations'>
+                    <img src={ PhotoDeProfil } alt='Photo de profil'/>
+                    <p id='profile-lastname'>Nom</p>
+                    <p id='profile-firstname'>Prénom</p>
+                </div>
+            </div>
+            <ul id='profile-section-nav'>
+                <li>All</li>
+                <li>Walls</li>
+                <li>Gallery</li>
+                <li>Video</li>
+                <li>Music</li>
+            </ul>
+        </>
+    )
+    
 }
--- src/pages/PageProfil.js
+++ src/pages/PageProfil.js
@@ -1,9 +1,11 @@
 import HeaderProfil from "../components/components-page/page-profil/HeaderProfil"
+import MainProfil from "../components/components-page/page-profil/MainProfil"
 
 export default function PageProfil(){
     return(
         <>
             <HeaderProfil/>
+            <MainProfil/>
         </>
     )
 }
